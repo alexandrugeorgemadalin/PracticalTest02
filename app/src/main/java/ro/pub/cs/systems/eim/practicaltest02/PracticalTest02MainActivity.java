275fb7c02018eb7dd309ea066c4be7373ec692e3
Alexandru George-Madalin <alexandrgeorge22@gmail.com>
2023-05-25 08:20:50 +0300

    1 si 2

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -3,11 +3,11 @@ package ro.pub.cs.systems.eim.practicaltest02;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PracticalTest02MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_practical_test02_main);
     }
 }
